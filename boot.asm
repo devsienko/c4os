3b65ddeda656f0c4d860788ae0fb2eb987c068c5
daniil <daniil_evsienko@mail.ru>
2021-09-15 00:43:25 +0400

    Added ListFS structures code

--- boot.asm
+++ boot.asm
@@ -2,6 +2,33 @@ org 0x7c00 ; We are loaded by BIOS at 0x7C00
 
 jmp start
 
+; ListFS header
+align 4
+fs_magic dd ?
+fs_version dd ?
+fs_flags dd ?
+fs_base dq ?
+fs_size dq ?
+fs_map_base dq ?
+fs_map_size dq ?
+fs_first_file dq ?
+fs_uid dq ?
+fs_block_size dd ?
+; ListFs file header
+virtual at 0x800
+f_info:
+    f_name rb 256
+    f_next dq ?
+    f_prev dq ?
+    f_parent dq ?
+    f_flags dq ?
+    f_data dq ?
+    f_size dq ?
+    f_ctime dq ?
+    f_mtime dq ?
+    f_atime dq ?
+end virtual
+
 boot_msg db "Hi there! My OS boot loader have started to work!",13,10,0
 
 write_str:
