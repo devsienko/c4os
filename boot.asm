78b50c2109785c298e3ca5ac811c3cf6ee4168d7
daniil <daniil_evsienko@mail.ru>
2021-09-15 10:26:02 +0400

    Added load_sector function

--- boot.asm
+++ boot.asm
@@ -35,7 +35,6 @@ label head_count byte at $$ + 2 ; number disk heads
 label disk_id byte at $$ + 3
 
 boot_msg db "Hi there! My OS boot loader have started to work!",13,10,0
-not_implemented_msg db "Not implemented",13,10,0
 
 ; write a string DS:SI to the monitor
 write_str:
@@ -51,24 +50,52 @@ write_str:
     pop si ax
     ret
 
-load_sector.error:
-	mov si, not_implemented_msg
-	call write_str
-	ret
-
 get_disk_parameters:
-	mov [disk_id], dl ; get bootable disk id
-	mov ah, 0x08
-	xor di, di ; ES is already zero
-	push es ; we don’t need a pointer to BIOS parameters block but we need zero ES
-	int 0x13
-	pop es
-	jc load_sector.error ; this function is not implemented for now
-	inc dh ; first heads number is 0, to get the length we need increment this one
-	mov [head_count], dh
-	and cx, 111111b ; select low 6 bits of CX
-	mov [sector_per_track], cx
-	ret
+    mov [disk_id], dl ; get bootable disk id
+    mov ah, 0x08
+    xor di, di ; ES is already zero
+    push es ; we don’t need a pointer to BIOS parameters block but we need zero ES
+    int 0x13
+    pop es
+    jc load_sector.error ; this function is not implemented for now
+    inc dh ; first heads number is 0, to get the length we need increment this one
+    mov [head_count], dh
+    and cx, 111111b ; select low 6 bits of CX
+    mov [sector_per_track], cx
+    ret
+
+; critical error
+error:
+    pop si
+    call write_str
+
+; load sector from DX:AX to buffer from ES:DI
+load_sector:
+    push ax bx cx dx si
+    div [sector_per_track] ; divide DX:AX by sector per track numbers (quotient will be written into AX (it's number of tracks), remainder of division into DX)
+    mov cl, dl ; remainder of division is sector index, we put it into CL
+    inc cl ; because first sector index is 1
+    div [head_count] ; we divide quotient (from AX) by disk head numbers (in this case we divide by byte instead of word)
+    mov dh, ah ; reminder of division (it’s in AH) is head number, we put it into DH
+    mov ch, al ; quotient (it’s in AL) is track number (aka cylinder number) - we put it into CH
+    mov dl, [disk_id] ; put disk id into DL
+    mov bx, di ; put the buffer offset into BX. The segment offset of BX is already in ES
+    mov al, 1 ; number of sectors for the reading - 1.
+    mov si, 3 ; number of writing attempts
+ @@: ; reading loop
+    mov ah, 2 ; routine number
+    int 0x13 ; try to read
+    jnc @f ; in success case go out from function
+    xor ah, ah ; reset routine number (0x00)
+    int 0x13 ; reset disk system
+    dec si ; decrement attempts count
+    jnz @b ; if we have not spent attempts then go to begin of the loop
+ .error: ; reading attempts are exceeded
+    call error
+    db "DISK ERROR",13,10,0
+ @@:
+    pop si dx cx bx ax
+    ret
 
 start:
 
