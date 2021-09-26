20df27d791ca112d250535e0e02a7fd700bba6db
devsienko <daniil_evsienko@mail.ru>
2021-09-26 22:23:05 +0400

    Files restructuring (moving kernel files to kernel folder)

--- kernel-build.bat
+++ kernel-build.bat
@@ -1,9 +1,9 @@
-fasm src\startup.asm bin\startup.o
+fasm src\kernel\startup.asm bin\startup.o
 SET OS_INCLUDE_PATH=src\include
 
 gcc src\lib\stdlib.c -o bin\stdlib.o -c -I %OS_INCLUDE_PATH%
-gcc src\kernel.c -o bin\main.o -c -I %OS_INCLUDE_PATH%
+gcc src\kernel\kernel.c -o bin\main.o -c -I %OS_INCLUDE_PATH%
 gcc src\drivers\tty.c -o bin\tty.o -c -I %OS_INCLUDE_PATH%
 
-ld -T src\kernel.ld -o bin\disk\kernel.bin bin\startup.o bin\stdlib.o bin\main.o bin\tty.o
+ld -T src\kernel\kernel.ld -o bin\disk\kernel.bin bin\startup.o bin\stdlib.o bin\main.o bin\tty.o
 objcopy bin\disk\kernel.bin -O binary
