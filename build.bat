beeee3770d5043dd30fc935183b5992ffcbbe707
devsienko <daniil_evsienko@mail.ru>
2023-12-29 22:15:06 +0300

    add bootloader stage2

--- build.bat
+++ build.bat
@@ -1,1 +1,11 @@
-fasm boot.asm boot.bios.bin
+if not exist "bin\" mkdir bin
+mkdir bin\disk
+
+del bin\* /Q
+
+fasm boot.asm bin\boot.bios.bin
+
+tools\dd if=bin\boot.bios.bin of=bin\boot_sector.bin bs=512 count=1
+tools\dd if=bin\boot.bios.bin of=bin\disk\boot.bin bs=1 skip=512
+
+make_listfs\make_listfs of=bin\disk.img bs=512 size=2880 boot=bin\boot_sector.bin src=.\bin\disk
